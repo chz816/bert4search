1bd15cb18105bf4d893303c2ad10eb344c22dd08
chz816 <czh097@gmail.com>
2019-11-05 16:51:33 -0500

    update

--- pre-train/augmentation/augmentation.sh
+++ pre-train/augmentation/augmentation.sh
@@ -1,14 +1,12 @@
-
-
 python run_classifier.py \
   --task_name=MRPC \
   --do_train=true \
   --do_eval=true \
-  --data_dir=./glue_data/MRPC \
+  --data_dir=./data/ \
   --vocab_file=./vocab.txt \
   --bert_config_file=./bert_config.json \
-  --init_checkpoint=./bert_model.ckpt \
-  --max_seq_length=128 \
+  --init_checkpoint=./data/bert_model.ckpt \
+  --max_seq_length=100 \
   --train_batch_size=32 \
   --learning_rate=2e-5 \
   --num_train_epochs=3.0 \
